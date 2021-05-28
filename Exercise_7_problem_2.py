bf3f669988621014b7b8d9d730ff2994df8ca8ed
kasai9244 <>
2021-05-28 07:19:16 +0000

    I added the code to load the Helsinki temperature data.

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -17,7 +17,9 @@
 # - Parse dates from the column `'DATE'` and set the dates as index in the dataframe 
 
 # YOUR CODE HERE 1 to read the data into data and parse dates
-
+import pandas as pd
+fp="helsinki-vantaa.csv"
+data= pd.read_csv(fp,delim_whitespace=True,parse_dates=['DATE'], index_col='DATE')
 # This test print should print first five rows
 print(data.head())
 
