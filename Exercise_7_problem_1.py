2c24c7604cba1d3a055cf3e9cd76fe415de0fe11
kasai9244 <>
2021-05-28 06:55:22 +0000

    I added the code to add a title, x-label and y-label to my plot.

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -60,10 +60,13 @@ assert len(colors) == 1000, "There should be 1000 random numbers for colors"
 # YOUR CODE HERE 3
 import matplotlib.pyplot as plt
 plt.scatter(x,y,s=50,c='black',cmap='rainbow',edgecolor = 'red')
-plt.show()
+
 # Add labels and title
 # YOUR CODE HERE 4
-
+plt.title("My random candy points")
+plt.xlabel("X-labels")
+plt.ylabel("Y-labels")
+plt.show()
 # Save the plot as a png file:
 outputfp = "my_first_plot.png"
 
