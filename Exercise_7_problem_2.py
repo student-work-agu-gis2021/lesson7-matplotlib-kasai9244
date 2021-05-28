8d8248fc3eef801a30f9043e28f2ffad2500b0b7
kasai9244 <>
2021-05-28 08:36:46 +0000

    I added the code to create a line plot that displays the temperatures (TEMP_C) for yeach month in the 30 year time period:

--- Exercise_7_problem_2.py
+++ Exercise_7_problem_2.py
@@ -53,7 +53,14 @@ print("Number of rows:", len(selection))
 # 
 
 # YOUR CODE HERE 3
-
+import matplotlib.pyplot as plt
+selection = selection.sort_index()
+plt.plot(selection.index,selection['TEMP_C'],linestyle='solid',c='black',marker='o',markersize=3)
+plt.title("Helsinki-Vantaa Airport")
+plt.xlabel("Time")
+plt.ylabel("Temperature(Celsius)")
+plt.grid()
+plt.show()
 # Set output file name
 outputfp = ""
 
